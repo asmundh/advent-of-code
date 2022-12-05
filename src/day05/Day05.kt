f22bf87ab2666598d718a4b1d39b431ae5cc353f
asmundh <aasmund.haugse@gmail.com>
2022-12-05 08:36:31 +0100

    Rewrite day5 to use stacks for both parts

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -2,8 +2,20 @@ package day05
 
 import readInput
 
+val startingStack = """
+    [T] [V]                     [W]
+    [V] [C] [P] [D]             [B]
+    [J] [P] [R] [N] [B]         [Z]
+    [W] [Q] [D] [M] [T]     [L] [T]
+    [N] [J] [H] [B] [P] [T] [P] [L]
+    [R] [D] [F] [P] [R] [P] [R] [S] [G]
+    [M] [W] [J] [R] [V] [B] [J] [C] [S]
+    [S] [B] [B] [F] [H] [C] [B] [N] [L]
+    1   2   3   4   5   6   7   8   9
+"""
+
 fun main() {
-    val startingCratePart1 = QCrate(
+    val startingCratePart1 = Crate(
         stacks = mutableListOf(
             ArrayDeque(listOf("S", "M", "R", "N", "W", "J", "V", "T")),
             ArrayDeque(listOf("B", "W", "D", "J", "Q", "P", "C", "V")),
@@ -16,68 +28,44 @@ fun main() {
             ArrayDeque(listOf("L", "S", "G")),
         )
     )
-    val startingCratePart2 = LCrate(
-        stacks = mutableListOf(
-            mutableListOf("S", "M", "R", "N", "W", "J", "V", "T"),
-            mutableListOf("B", "W", "D", "J", "Q", "P", "C", "V"),
-            mutableListOf("B", "J", "F", "H", "D", "R", "P"),
-            mutableListOf("F", "R", "P", "B", "M", "N", "D"),
-            mutableListOf("H", "V", "R", "P", "T", "B"),
-            mutableListOf("C", "B", "P", "T"),
-            mutableListOf("B", "J", "R", "P", "L"),
-            mutableListOf("N", "C", "S", "L", "T", "Z", "B", "W"),
-            mutableListOf("L", "S", "G"),
-        )
-    )
 
     fun part1(input: List<String>): String {
-        val crate = startingCratePart1.copy()
+        val crate = Crate(
+            stacks = startingCratePart1.stacks.map { it -> ArrayDeque(it.map { it }) }.toMutableList()
+        )
         input.forEach { ops ->
-            val moveOperations = ops
-                .filter { it.isDigit() || it.isWhitespace() }
-                .split("  ")
-                .map { it.replace(" ", "") }
-                .map { it.toInt() }
-            crate.move(moveOperations[0], moveOperations[1], moveOperations[2])
+            val moveOperations = ops.toOperations()
+            crate.moveOneAtATime(moveOperations[0], moveOperations[1], moveOperations[2])
         }
-        return crate.stacks.map { it.last() }.joinToString("")
+        return crate.stacks.joinToString("") { it.last() }
     }
 
     fun part2(input: List<String>): String {
-        val crate = startingCratePart2.copy()
+        val crate = Crate(
+            stacks = startingCratePart1.stacks.map { it -> ArrayDeque(it.map { it }) }.toMutableList()
+        )
         input.forEach { ops ->
-            val moveOperations = ops
-                .filter { it.isDigit() || it.isWhitespace() }
-                .split("  ")
-                .map { it.replace(" ", "") }
-                .map { it.toInt() }
-            println(moveOperations)
-            crate.move(moveOperations[0], moveOperations[1], moveOperations[2])
+            val moveOperations = ops.toOperations()
+            crate.moveMultiple(moveOperations[0], moveOperations[1], moveOperations[2])
         }
-        return crate.stacks.map { it.last() }.joinToString("")
+        return crate.stacks.joinToString("") { it.last() }
     }
-
-    val startingStack = """
-    [T] [V]                     [W]
-    [V] [C] [P] [D]             [B]
-    [J] [P] [R] [N] [B]         [Z]
-    [W] [Q] [D] [M] [T]     [L] [T]
-    [N] [J] [H] [B] [P] [T] [P] [L]
-    [R] [D] [F] [P] [R] [P] [R] [S] [G]
-    [M] [W] [J] [R] [V] [B] [J] [C] [S]
-    [S] [B] [B] [F] [H] [C] [B] [N] [L]
-    1   2   3   4   5   6   7   8   9
-    """
 
     val input = readInput("Day05")
     println(part1(input))
     println(part2(input))
 }
 
-data class QCrate(
+fun String.toOperations() =
+    filter { it.isDigit() || it.isWhitespace() }
+    .split("  ")
+    .map { it.replace(" ", "") }
+    .map { it.toInt() }
+
+data class Crate(
     val stacks: MutableList<ArrayDeque<String>>
 ) {
-    fun move(amount: Int, fromPosition: Int, toPosition: Int) {
+    fun moveOneAtATime(amount: Int, fromPosition: Int, toPosition: Int) {
         val moveFrom = fromPosition - 1
         val moveTo = toPosition - 1
         repeat(amount) {
@@ -85,17 +73,14 @@ data class QCrate(
             stacks[moveTo].addLast(poppedValue)
         }
     }
-}
-data class LCrate(
-    val stacks: MutableList<MutableList<String>>
-) {
-    fun move(amount: Int, fromPosition: Int, toPosition: Int) {
+
+    fun moveMultiple(amount: Int, fromPosition: Int, toPosition: Int) {
         val moveFrom = fromPosition - 1
         val moveTo = toPosition - 1
-        val poppedValues = stacks[moveFrom].takeLast(amount)
+        val toMove = ArrayDeque<String>()
         repeat(amount) {
-            stacks[moveFrom].removeLast()
+            toMove.addFirst(stacks[moveFrom].removeLast())
         }
-        stacks[moveTo].addAll(poppedValues)
+        stacks[moveTo].addAll(toMove)
     }
 }
