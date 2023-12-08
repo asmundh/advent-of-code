a3a73d2de3161cc52f4e89dd9c2ad786a6734b6c
asmundh <aasmund.haugse@gmail.com>
2023-12-08 13:11:38 +0100

    Day 8

---
+++ src/main/kotlin/aoc23/Day08.kt
@@ -0,0 +1,76 @@
+package aoc23
+
+import lcm
+import runTask
+import utils.InputReader
+import java.lang.IllegalStateException
+import kotlin.collections.Map
+
+fun day8part1(input: List<String>): Int {
+    val m = input.toSwampMap()
+    var current = "AAA"
+    var index = 0
+    var moves = 0
+    while (current != "ZZZ") {
+        current = when (m.instructions[index++]) {
+            'L' -> m.maps[current]?.left!!
+            'R' -> m.maps[current]?.right!!
+            else -> throw IllegalStateException("What the hell")
+        }
+        index %= m.instructions.length
+        moves ++
+    }
+    return moves
+}
+
+fun day8part2(input: List<String>): Long {
+    val swampMap = input.toSwampMap()
+    val locations = swampMap.maps.keys.filter { it.last() == 'A' }
+    var index = 0
+
+    val goals = mutableListOf<Long>()
+
+    locations.forEach {
+        var current = it
+        var moves = 0L
+        while (current.last() != 'Z') {
+            current = when (swampMap.instructions[index++]) {
+                'L' -> swampMap.maps[current]?.left!!
+                'R' -> swampMap.maps[current]?.right!!
+                else -> throw IllegalStateException("What the hell")
+            }
+            index %= swampMap.instructions.length
+            moves ++
+        }
+        goals.add(moves)
+    }
+    return goals.reduce { acc, num -> lcm(acc, num) }
+}
+
+data class SwampMap(
+    val instructions: String,
+    val maps: Map<String, Target>
+)
+
+data class Target(
+    val left: String,
+    val right: String,
+)
+
+fun List<String>.toSwampMap(): SwampMap =
+    SwampMap(
+        this.first(),
+        this.drop(2).associate {
+            val target = it.substring(0, 3)
+            val left = it.substring(7, 10)
+            val right = it.substring(12, 15)
+
+            target to Target(left, right)
+        }
+    )
+
+fun main() {
+    val input = InputReader.getInputAsList<String>(8)
+//    runTask("D8P1") { day8part1(input) }
+    runTask("D8P2") { day8part2(input) }
+}
--- src/main/kotlin/template/Dayxx.kt
+++ src/main/kotlin/template/Dayxx.kt
@@ -3,16 +3,16 @@ package template
 import runTask
 import utils.InputReader
 
-fun part1(input: List<String>): Int {
+fun dayxpart1(input: List<String>): Int {
     return 0
 }
 
-fun part2(input: List<String>): Int {
+fun dayxpart2(input: List<String>): Int {
     return 1
 }
 
 fun main() {
     val input = InputReader.getInputAsList<String>(1)
-    runTask("DxP1") { part1(input) }
-    runTask("DxP2") { part2(input) }
+    runTask("DxP1") { dayxpart1(input) }
+    runTask("DxP2") { dayxpart2(input) }
 }
